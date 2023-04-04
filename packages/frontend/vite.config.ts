76d880b322068eb6a5ed92e7d4623c74b8a049e6
Sandeep Nambiar <sandeep.turing@gmail.com>
2023-04-04 02:24:08 +0200

    feat: fix api path

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3010',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path,
       },
     }
   }
