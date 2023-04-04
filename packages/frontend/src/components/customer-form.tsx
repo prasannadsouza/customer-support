9ec3fe4fb6acfe5acc0c4dd726dd4cbc268e85c8
Prasanna D'souza <prasannadsouza@gmail.com>
2023-04-04 03:00:10 +0200

    feat: customer form

--- packages/frontend/src/components/customer-form.tsx
+++ packages/frontend/src/components/customer-form.tsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 
 type SuccessResponse = {
-  id: number
+    id: number
 };
 
 export const CustomerForm = () => {
-  const [loading, setLoading] = useState(false);
-  const [lastId, setLastId] = useState<number|null>(null);
+    const [loading, setLoading] = useState(false);
+    const [lastId, setLastId] = useState<number | null>(null);
 
-  const submit = async (e: any) => {
-    e.preventDefault();
+    const submit = async (e: any) => {
+        e.preventDefault();
         if (loading) {
             return;
         }
 
-    setLoading(true);
-    setLastId(null);
-    try {
-      const form = e.target;
-      const formData = new FormData(form);
-      const data = Object.fromEntries(formData.entries());
-      console.log(data);
+        setLoading(true);
+        setLastId(null);
+        try {
+            const form = e.target;
+            const formData = new FormData(form);
+            const data = Object.fromEntries(formData.entries());
+            console.log(data);
             const url = '/api/tickets';
             const resp = await fetch(url, {
                 method: "POST",
@@ -29,10 +29,10 @@ export const CustomerForm = () => {
                 },
                 body: JSON.stringify(data),
             });
-      const val = await resp.json() as SuccessResponse;
-      console.log(val);
-      form.reset();
-      setLastId(val.id);
+            const val = await resp.json() as SuccessResponse;
+            console.log(val);
+            form.reset();
+            setLastId(val.id);
         }
         catch (ex: any) {
             console.log(ex);
@@ -49,7 +49,7 @@ export const CustomerForm = () => {
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
                         Item Name
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="subject" type="text" required/>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="subject" type="text" required />
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
@@ -57,7 +57,7 @@ export const CustomerForm = () => {
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
                         E-mail
                     </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" name="createdByEmail" type="email" required/>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" name="createdByEmail" type="email" required />
                 </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
@@ -70,9 +70,9 @@ export const CustomerForm = () => {
             </div>
             <div className="flex items-center justify-between">
                 <button className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                        Send
-                    </button>
-              <div className="">{lastId ? `Your ticket number is ${lastId}` : ""}</div>
+                    Send
+                </button>
+                <div className="">{lastId ? `Your ticket number is ${lastId}` : ""}</div>
             </div>
         </form>
     );
