329017a72a1de183a092e063836bf2e32a3d00ff
Prasanna D'souza <prasannadsouza@gmail.com>
2023-04-04 03:00:23 +0200

    feat: fix api path

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT);
 }
 bootstrap();
